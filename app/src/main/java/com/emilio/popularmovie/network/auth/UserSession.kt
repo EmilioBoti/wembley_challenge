4bbd74f24e658a18828c7b306ed7d6c3978cd16d
Emilio Botier <70599464+EmilioBoti@users.noreply.github.com>
2023-03-07 21:52:05 +0100

    config api-key

--- app/src/main/java/com/emilio/popularmovie/network/auth/UserSession.kt
+++ app/src/main/java/com/emilio/popularmovie/network/auth/UserSession.kt
@@ -1,10 +1,11 @@
 package com.emilio.popularmovie.network.auth
 
 import android.content.Context
+import com.emilio.popularmovie.BuildConfig
 import com.emilio.popularmovie.R
 
 object UserSession {
-    private const val API_KEY = "f4d9eef562ddac8945db4d955d7a86c5"
+    private const val API_KEY = BuildConfig.SECRET_APIKEY
     const val SESSION_ID = "session_id"
     const val ACCOUNT_ID = "account_id"
     const val USERNAME = "username"
@@ -22,7 +23,7 @@ object UserSession {
             ?.clear()
             ?.apply()
     }
-
+    
     fun getSession(context: Context?): MutableMap<String, *>? {
         return context?.getSharedPreferences(context.getString(R.string.user_session), Context.MODE_PRIVATE)?.all
     }
