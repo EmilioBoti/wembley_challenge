a88cfab3fe5e696a1c6216068feeecd537ab15c0
Emilio Botier <70599464+EmilioBoti@users.noreply.github.com>
2023-03-07 20:47:51 +0100

    added navigation pages

--- app/src/main/java/com/emilio/popularmovie/domain/movieList/MovieListViewModel.kt
+++ app/src/main/java/com/emilio/popularmovie/domain/movieList/MovieListViewModel.kt
@@ -4,13 +4,13 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.emilio.popularmovie.model.FavoriteBody
 import com.emilio.popularmovie.model.Movie
 import com.emilio.popularmovie.model.Page
-import com.emilio.popularmovie.network.auth.SessionStatus
 import com.emilio.popularmovie.network.auth.UserSession
 import com.emilio.popularmovie.service.IRepository
-import com.emilio.popularmovie.service.Status
+import kotlinx.coroutines.launch
 
 class MovieListViewModel(private val repository: IRepository): ViewModel(), IMovie, ISession {
     private var _listMovies: MutableLiveData<MutableList<Movie>> = MutableLiveData()
@@ -19,44 +19,54 @@ class MovieListViewModel(private val repository: IRepository): ViewModel(), IMov
     private var session: MutableMap<String, *>? = null
 
 
-    override suspend fun getMovies(page: Int) {
+    override fun getMovies(page: Int) {
         val params = UserSession.getApiKeyParam().apply {
             put("page", page.toString())
         }
 
-        val res = repository.retrieveMovies(params)
-        res?.let {
-            currentPage = it
-            updateList(it.results)
+        viewModelScope.launch {
+            val res = repository.retrieveMovies(params)
+            res?.let {
+                currentPage = it
+                updateList(it.results)
+            }
         }
 
     }
 
-    override suspend fun getMoviesDetail(pos: Int) {
+    override fun getMoviesDetail(pos: Int) {
         val movie = listMovies.value?.get(pos)
     }
 
-    override suspend fun addToFavorite(pos: Int) {
+    override fun addToFavorite(pos: Int) {
         val movie = listMovies.value?.get(pos)
 
         session?.let {
-            val params = UserSession.getApiKeyParam().apply {
-                put("session_id", it[UserSession.SESSION_ID] as String)
-            }
-            val status: Status? = movie?.id?.let { movieId ->
-                repository.markFavoriteService(it[UserSession.ACCOUNT_ID] as Int, FavoriteBody("movie", movieId, true), params)
+            viewModelScope.launch {
+                val params = UserSession.getApiKeyParam().apply {
+                    put("session_id", it[UserSession.SESSION_ID] as String)
+                }
+                movie?.id?.let { movieId ->
+                    addAndRemoveToFavorite(it[UserSession.ACCOUNT_ID] as Int, FavoriteBody("movie", movieId, true), params)
+                }
             }
         }
     }
 
-    override suspend fun searchMovie(name: String) {
+    private suspend fun addAndRemoveToFavorite(accountId: Int, favBody: FavoriteBody, params: HashMap<String, String>) {
+        repository.markFavoriteService(accountId, favBody, params)
+    }
+
+    override fun searchMovie(name: String) {
         val params = UserSession.getApiKeyParam().apply {
             put("query", name)
         }
 
-        val res = repository.searchMovies(params)
-        res?.let {
-            updateList(it.results)
+        viewModelScope.launch {
+            val res = repository.searchMovies(params)
+            res?.let {
+                updateList(it.results)
+            }
         }
     }
 
@@ -72,5 +82,26 @@ class MovieListViewModel(private val repository: IRepository): ViewModel(), IMov
         session = UserSession.getSession(context)
     }
 
+    fun previousPage() {
+        currentPage?.let {
+            val p = it.page-1
+            changePage(p)
+        }
+    }
+
+    fun nextPage() {
+        currentPage?.let {
+            val p = it.page+1
+            changePage(p)
+        }
+    }
+
+    private fun changePage(page: Int) {
+        if (page in  0..10000) {
+            viewModelScope.launch {
+                getMovies(page)
+            }
+        }
+    }
 
 }
